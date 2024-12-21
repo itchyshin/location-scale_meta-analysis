4bfaf9d56c6229a9a48098270f4009df92118462
Shinichi Nakagawa <itchyshin@gmail.com>
2024-12-21 13:29:22 -0700

    data

---
+++ R/test.R
@@ -0,0 +1,36 @@
+# some teests 
+
+pacman::p_load(tidyverse, 
+               here,
+               patchwork,
+               ggplot2,
+               pander
+)
+
+############
+# load data
+############
+
+
+
+# patrice's data set (categoical - biological)
+
+
+
+# Julia's data set (ctegorical - method)
+
+
+
+
+# latitiude - (continious)
+
+dat <- read.csv(here("data", "latitude.csv"))
+
+
+
+
+
+# plant data set
+#data <- read_csv(here("data", "plant_dat.csv"))
+
+# phylo?
